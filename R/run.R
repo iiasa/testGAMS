8ceebcc5cb0abb1ab74a6bb7cbfffe282a130f5b
Albert Brouwer <brouwer@iiasa.ac.at>
2020-09-18 10:15:29 +0200

    Pass parameters to GAMS via a parameter files
    This is more robust as it avoids platform-dependent shell escaping and command line length limits.

--- R/run.R
+++ R/run.R
@@ -13,6 +13,8 @@ run <- function(script, re_dir) {
   path <- fs::as_fs_path(script)
   stopifnot(fs::is_file(script))
   stopifnot(fs::path_ext(script) == "gms")
+  re_dir <- fs::as_fs_path(re_dir)
+  stopifnot(fs::dir_exists(re_dir))
 
   # Extract the GAMS file name without extension from the path
   name <- fs::path_ext_remove(fs::path_file(script))
@@ -20,9 +22,20 @@ run <- function(script, re_dir) {
   # Get path to GAMS binary (no exe extension needed)
   gams <- fs::path(get_sys_dir(), "gams")
 
+  # Construct a parameter file for GAMS (more robust)
+  parameter_templates = c(
+    'output="{fs::path(re_dir, \'output.lst\')}"'
+  )
+  parameters <- purrr::map_chr(parameter_templates, stringr::str_glue, .envir=environment())
+  parameter_file <- fs::path(re_dir, "parameters.txt")
+  parameter_conn<-file(parameter_file, open="wt")
+  writeLines(parameters, parameter_conn)
+  close(parameter_conn)
+
   # Construct command line arguments for GAMS
   arg_templates = c(
-    '"{script}"'
+    '"{script}"',
+    '-parmFile "{parameter_file}"'
   )
   args <- purrr::map_chr(arg_templates, stringr::str_glue, .envir=environment())
 

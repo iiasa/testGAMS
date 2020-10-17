14ed12a76dbcbb618181ece4fe016e298bce8317
Albert Brouwer <brouwer@iiasa.ac.at>
2020-10-17 18:07:41 +0200

    Add tests for gdx_* functions

---
+++ tests/testthat/test-gdx.R
@@ -0,0 +1,71 @@
+test_that("gdx_info() can index a GDX", {
+  re_dir <- local_re_dir()
+  script <- fs::file_create(fs::path(re_dir, "test.gms"))
+  conn <- file(script, open="wt")
+  writeLines(c(
+      'display "Do nothing.";'
+    ), conn)
+  close(conn)
+  code <- run(script, re_dir)
+  expect_equal(code, CODE_NORMAL_RETURN)
+
+  info <- gdx_info(re_dir)
+  expect_true('gdxLibraryVer' %in% names(info))
+  expect_true('gdxFileVer' %in% names(info))
+  expect_true('producer' %in% names(info))
+  expect_true('symCount' %in% names(info))
+  expect_true('uelCount' %in% names(info))
+  expect_true('sets' %in% names(info))
+  expect_true('parameters' %in% names(info))
+  expect_true('variables' %in% names(info))
+  expect_true('equations' %in% names(info))
+  expect_true('aliases' %in% names(info))
+  expect_true('path' %in% names(info))
+})
+
+test_that("gdx_parameter() can read a parameter from a GDX", {
+  re_dir <- local_re_dir()
+  script <- fs::file_create(fs::path(re_dir, "test.gms"))
+  conn <- file(script, open="wt")
+  writeLines(c(
+    'Parameter',
+    '    dd(*) "distribution of demand"',
+    '        / mexico-df   55,',
+    '          guadalaja   15 /;'
+  ), conn)
+  close(conn)
+  code <- run(script, re_dir)
+  expect_equal(code, CODE_NORMAL_RETURN)
+
+  info <- gdx_info(re_dir)
+  p <- gdx_parameter(info, 'dd')
+  expect_true(tibble::is_tibble(p))
+  expect_true('.i' %in% names(p))
+  expect_true(p[[1]][[1]] == 'mexico-df')
+  expect_true(p[[2]][[2]] ==  15)
+})
+
+test_that("gdx_set() can read a parameter from a GDX", {
+  re_dir <- local_re_dir()
+  script <- fs::file_create(fs::path(re_dir, "test.gms"))
+  conn <- file(script, open="wt")
+  writeLines(c(
+    'Set cf "final products" /',
+    'syncrude "refined crude (mil bbls)"',
+    'lpg      "liquefied petroleum gas (million bbls)"',
+    'ammonia  "ammonia (mil tons)"',
+    'coke     "coke (mil tons)"',
+    'sulfur   "sulfur (mil tons)" /;'
+  ), conn)
+  close(conn)
+  code <- run(script, re_dir)
+  expect_equal(code, CODE_NORMAL_RETURN)
+
+  info <- gdx_info(re_dir)
+  s <- gdx_set(info, 'cf')
+  expect_true(is.list(s))
+  expect_true('.i' %in% names(s))
+  expect_true('.te' %in% names(s))
+  expect_true(s[[1]][[1]] == 'syncrude')
+  expect_true(s[[2]][[5]] ==  "sulfur (mil tons)")
+})

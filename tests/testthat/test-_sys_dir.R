7ad70838f9373024f50fd82c61086f922078015f
Albert Brouwer <brouwer@iiasa.ac.at>
2020-10-30 13:49:45 +0100

    Improve test context and description

--- tests/testthat/test-_sys_dir.R
+++ tests/testthat/test-_sys_dir.R
@@ -1,12 +1,12 @@
-# The underscore in the test filename ensures this is the first test
+# The underscore in the test file name ensures this is the first test
 # to run since further tests depend on the GAMS system directory
 # being available. Tests run in name sort order.
 #
 # This test will fail when GAMS the system directory was never set
 # and no GAMS system directory is set in either R_GAMS_SYSDIR or
 # the PATH|LD_LIBRARY_PATH environment variable.
-context("Try a context for sys dir")
-test_that("GAMS system directory can be gotten and set", {
+context("sys_dir")
+test_that("get_sys_dir() returns what is set by set_sys_dir() ", {
   sys_dir <- get_sys_dir()
   set_sys_dir(sys_dir)
   expect_equal(sys_dir, get_sys_dir())

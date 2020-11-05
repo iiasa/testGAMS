8b44d4b3017147a289a233e5bc04a6ae1f06a871
Albert Brouwer <brouwer@iiasa.ac.at>
2020-11-05 14:35:47 +0100

    Add more read_* functions
    read_par() to reference the GAMS parameter file used for run().
    read_trace_report() and read_trace_summary() to pick up results of report_trace().
    read_trace_log() to see why report_trace() produced an error code.

--- R/read.R
+++ R/read.R
@@ -12,9 +12,20 @@ read_lines <- function(path) {
   lines
 }
 
+#' Read lines from parameter file
+#'
+#' Reads the lines of the GAMS parameter file used for a [run()].
+#'
+#' @param  re_dir Redirection directory used for the [run()].
+#' @return Character vector holding lines in the read parameter file.
+#' @export
+read_par <- function(re_dir) {
+  read_lines(fs::path(re_dir, PAR_FILE_NAME))
+}
+
 #' Read lines from log file
 #'
-#' Reads the lines from the GAMS log output file written during a [run()].
+#' Reads the lines of the GAMS log output file written during a [run()].
 #'
 #' @param  re_dir Redirection directory used for the [run()].
 #' @return Character vector holding lines in the read log file.
@@ -25,7 +36,7 @@ read_log <- function(re_dir) {
 
 #' Read lines from listing file
 #'
-#' Reads the lines from the GAMS listing output file written during a [run()].
+#' Reads the lines of the GAMS listing output file written during a [run()].
 #'
 #' @param  re_dir Redirection directory used for the [run()].
 #' @return Character vector holding lines in the read listing file.
@@ -36,11 +47,46 @@ read_lst <- function(re_dir) {
 
 #' Read lines from trace file
 #'
-#' Reads the lines from the GAMS trace file written during a [run()].
+#' Reads the lines of the GAMS trace file written during a [run()].
 #'
 #' @param  re_dir Redirection directory used for the [run()].
 #' @return Character vector holding lines in the read trace file.
 #' @export
 read_trace <- function(re_dir) {
   read_lines(fs::path(re_dir, TRACE_FILE_NAME))
+}
+
+#' Read lines from trace log file
+#'
+#' Reads the lines of the GAMS trace file written during [report_trace()].
+#' Examine at the trace log file when [report_trace()] returns an error code.
+#'
+#' @param  re_dir Redirection directory used for the [run()].
+#' @return Character vector holding lines in the read trace log file.
+#' @export
+read_trace_log <- function(re_dir) {
+  read_lines(fs::path(re_dir, TRACE_LOG_FILE_NAME))
+}
+
+
+#' Read lines from trace report file
+#'
+#' Reads the lines of the GAMS trace report file written during [report_trace()].
+#'
+#' @param  re_dir Redirection directory used for the [run()].
+#' @return Character vector holding lines in the read trace report file.
+#' @export
+read_trace_report <- function(re_dir) {
+  read_lines(fs::path(re_dir, TRACE_REPORT_FILE_NAME))
+}
+
+#' Read lines from trace summary file
+#'
+#' Reads the lines of the GAMS trace summary file written during [report_trace()].
+#'
+#' @param  re_dir Redirection directory used for the [run()].
+#' @return Character vector holding lines in the read trace summary file.
+#' @export
+read_trace_summary <- function(re_dir) {
+  read_lines(fs::path(re_dir, TRACE_SUMMARY_FILE_NAME))
 }
